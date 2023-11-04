233d4b5fc130a373e2cfffd9fdd4e52440192020
François Perin <f.perin@smartway.ai>
2023-11-04 17:35:36 +0100

    :lipstick: Override default theme

--- index.js
+++ index.js
@@ -2,15 +2,27 @@
  * @format
  */
 
-import React, {AppRegistry} from 'react-native';
-import {PaperProvider} from 'react-native-paper';
+import React, { AppRegistry } from 'react-native';
+import {
+  MD3LightTheme as DefaultTheme,
+  PaperProvider
+} from 'react-native-paper';
 import App from './src/App';
-import {name as appName} from './src/app.json';
+import { name as appName } from './src/app.json';
 import 'reflect-metadata'; // Need for inversify
+
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    "primary": '#466da3',
+    "surfaceVariant": '#abcaf5',
+  },
+};
 
 export default function Main() {
   return (
-    <PaperProvider>
+    <PaperProvider theme={theme}>
       <App />
     </PaperProvider>
   );
